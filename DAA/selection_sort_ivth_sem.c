4d0dfcc30ae6b418f3732d802d773976a2091ca9
VAISHNAVI GUPTA <guptavaishnavi76002@gmail.com>
2026-06-12 03:59:39 +0000

    add

--- DAA/selection_sort_ivth_sem.c
+++ DAA/selection_sort_ivth_sem.c
@@ -1,5 +1,43 @@
 #include<stdio.h>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void selection_sort(int arr[], int n) {
     int i, j, min_idx;
     for (i = 0; i < n - 1; i++) {
