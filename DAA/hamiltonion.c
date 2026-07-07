429c82ae9dbe5cfff64ecc04001b49da29bb0dd5
VAISHNAVI GUPTA <guptavaishnavi76002@gmail.com>
2026-07-07 15:00:09 +0000

    add

--- DAA/hamiltonion.c
+++ DAA/hamiltonion.c
@@ -58,7 +58,6 @@ int main()
     printf("Enter adjacency matrix:\n");
     for(i=1;i<=n;i++)
         for(j=1;j<=n;j++)
-
             scanf("%d",&g[i][j]);
     x[1] = 1;   
     printf("All possible Solutions:\n");
