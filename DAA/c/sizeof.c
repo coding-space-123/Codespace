6f7b11e21ecc0f6bf4c1306d4f576782e75d3d0d
coding-space-123 <guptavaishnavi76002@gmail.com>
2026-07-16 18:27:24 +0530

    add

--- DAA/c/sizeof.c
+++ DAA/c/sizeof.c
@@ -47,8 +47,6 @@ int main()
 
 
 
-
-
     printf("size of int array =%u\n",sizeof(a[20]));
     printf("size of float array =%u\n",sizeof(b[20]));
     printf("size of char array =%u\n",sizeof(c[20]));
