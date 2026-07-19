60e70c39aa4c35a0d685606d3f7576e932f9a438
coding-space-123 <guptavaishnavi76002@gmail.com>
2026-07-19 21:34:16 +0530

    add

--- java_project/AddThreeNumbers.java
+++ java_project/AddThreeNumbers.java
@@ -4,6 +4,12 @@ public class AddThreeNumbers {
         int a = 10;
         int b = 20;
         int c = 30;
+
+
+
+
+
+        
         int sum = a + b + c;
         System.out.println("Sum of three numbers: " + sum);
     }
