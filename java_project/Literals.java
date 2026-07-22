f03296154830fb6f7d56391b4031cba55a4e96fb
coding-space-123 <guptavaishnavi76002@gmail.com>
2026-07-22 18:15:56 +0530

    add

--- java_project/Literals.java
+++ java_project/Literals.java
@@ -18,7 +18,6 @@ public class Literals {
         // double Literal
         double doubleLiteral = 12345.6789;
         System.out.println("Double Literal: " + doubleLiteral);
-
         // Character Literal
         char charLiteral = 'A';
         System.out.println("Character Literal: " + charLiteral);
