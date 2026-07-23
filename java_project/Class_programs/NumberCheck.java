704d179087ad065a6163ee7414c77a7da177f62a
coding-space-123 <guptavaishnavi76002@gmail.com>
2026-07-23 19:34:19 +0530

    add

--- java_project/Class_programs/NumberCheck.java
+++ java_project/Class_programs/NumberCheck.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class NumberCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
         int temp = num, reverse = 0, strongSum = 0, armSum = 0;
