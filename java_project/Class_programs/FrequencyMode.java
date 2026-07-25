1d2ee894390bc2db6f834bad9a4432f361c8902c
coding-space-123 <guptavaishnavi76002@gmail.com>
2026-07-25 21:06:56 +0530

    add

--- java_project/Class_programs/FrequencyMode.java
+++ java_project/Class_programs/FrequencyMode.java
@@ -32,8 +32,6 @@ public class FrequencyMode {
                 }
             }
 
-
-
             System.out.println(arr[i] + " -> Frequency = " + count);
             if (count == 1)
                 System.out.println(arr[i] + " is Unique");
