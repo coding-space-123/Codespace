fbeb76450b5c5e062bc1545bed89d0f17f98d9b4
coding-space-123 <guptavaishnavi76002@gmail.com>
2026-07-26 23:08:10 +0530

    add

---
+++ java_project/Class_programs/String6.java
@@ -0,0 +1,19 @@
+class String6 {
+    public static void main(String args[]) {
+        String s = "  Java Programming  ";
+        String s1 = "";
+        String s2 = "   ";
+
+        System.out.println("Original String : '" + s + "'");
+
+        System.out.println("Uppercase : " + s.toUpperCase());
+
+        System.out.println("Lowercase : " + s.toLowerCase());
+
+        System.out.println("After trim() : '" + s.trim() + "'");
+
+        System.out.println("isEmpty() on s1 : " + s1.isEmpty());
+
+        System.out.println("isBlank() on s2 : " + s2.isBlank());
+    }
+}
