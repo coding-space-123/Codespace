b16137faee61fa3c2c0038394e50e87364b522ec
coding-space-123 <guptavaishnavi76002@gmail.com>
2026-07-30 21:09:30 +0530

    add

--- java_project/Class_programs/RotateArray.java
+++ java_project/Class_programs/RotateArray.java
@@ -4,7 +4,6 @@ public class RotateArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
         System.out.print("Enter size of array: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
